974bcc46ef9ab87048cf8a7780d4d5db86525fba
林浒 <7971132+lin-hu@user.noreply.gitee.com>
2021-08-14 12:32:27 +0800

    1.0版本

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
+/**
+ * 项目的启动入口
+ */
+
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+// 加载 element 组件库
+import ElementUI from 'element-ui'
+
+// 加载 element 组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 加载全局样式文件
+import './styles/index.less'
+
+// 全局注册 element 组件库
+Vue.use(ElementUI)
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/fans/index.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+    <h1>粉丝管理</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fans'
+
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
